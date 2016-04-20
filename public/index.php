e51bf107754c2f57d010324bbbc7bd166de03855
Kood Sonpe <patrick.odunlami@gmail.com>
2016-04-19 22:21:24 -0400

    Add comment on hotfix branch

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
- * @package  Laravel Blog by podunlami Leboss
+ * @package  Laravel Blog by podunlami Leboss On branch Hotfix
  * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
 
